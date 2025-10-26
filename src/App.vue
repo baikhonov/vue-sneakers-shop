dd785703a29b3bc3d1c91d6cf31ae7e92d168593
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2025-10-27 00:53:03 +0500

    Сверстал хедер

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup></script>
+<script setup>
+import PageHeader from './components/PageHeader.vue'
+</script>
 
 <template>
-  <h1>It works!</h1>
+  <div class="w-4/5 h-screen mx-auto mt-14 bg-white rounded-xl shadow-xl">
+    <PageHeader />
+    <h1>Samrat is a cool web developer</h1>
+  </div>
 </template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="flex justify-between px-10 py-8 border-b border-b-[#eaeaea]">
+    <div class="flex items-center gap-x-4">
+      <img class="w-10" src="/logo.png" alt="Logo" />
+      <div>
+        <p class="font-bold text-xl uppercase">Vue Sneakers</p>
+        <p class="text-sm text-gray-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+    <ul class="flex gap-8">
+      <li class="flex items-center gap-2 text-gray-600 hover:text-black cursor-pointer">
+        <img src="/cart.svg" alt="Корзина" />
+        <span class="font-semibold">1205 руб.</span>
+      </li>
+      <li class="flex items-center gap-2 text-gray-600 hover:text-black cursor-pointer">
+        <img src="/heart.svg" alt="Закладки" />
+        <span>Закладки</span>
+      </li>
+      <li class="flex items-center gap-2 text-gray-600 hover:text-black cursor-pointer">
+        <img src="/profile.svg" alt="Профиль" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
