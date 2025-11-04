e39b8a3776f45e47d668e03517ff4cf950d15c15
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2025-11-04 23:36:32 +0500

    Реализовал вывод заглушки в случае пустой корзины

---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: String,
+  description: String,
+  imageUrl: String,
+})
+</script>
+
+<template>
+  <div class="w-72 mx-auto flex flex-col items-center text-center">
+    <img w="70" height="70" class="max-w-[120px] mb-5" :src="imageUrl" :alt="title" />
+    <h2 class="mb-2 font-semibold text-2xl">{{ title }}</h2>
+    <p class="text-center text-gray-400 text-base/[150%]">{{ description }}</p>
+  </div>
+</template>
--- src/components/PageCart.vue
+++ src/components/PageCart.vue
@@ -1,7 +1,8 @@
 <script setup>
 import CartHeader from '@/components/CartHeader.vue'
 import CartList from '@/components/CartList.vue'
-import { inject, computed } from 'vue'
+import { inject } from 'vue'
+import InfoBlock from '@/components/InfoBlock.vue'
 
 const { closeCart } = inject('cart')
 
@@ -19,26 +20,37 @@ defineProps({
   <div @click="closeCart" class="fixed z-1 top-0 left-0 w-full h-full bg-black opacity-50"></div>
   <div class="fixed z-2 top-0 right-0 w-96 h-full p-8 bg-white flex flex-col">
     <CartHeader />
-    <CartList />
 
-    <div class="flex flex-col gap-5">
-      <div class="flex gap-2">
-        <span>Итого:</span>
-        <span class="flex-1 border-b border-dashed border-[#dfdfdf]"></span>
-        <span class="font-semibold">{{ totalPrice }} ₽</span>
+    <div v-if="!totalPrice" class="grow flex items-center">
+      <InfoBlock
+        image-url="/package-icon.png"
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+      />
+    </div>
+
+    <div v-else class="flex flex-col grow">
+      <CartList />
+
+      <div class="flex flex-col gap-5">
+        <div class="flex gap-2">
+          <span>Итого:</span>
+          <span class="flex-1 border-b border-dashed border-[#dfdfdf]"></span>
+          <span class="font-semibold">{{ totalPrice }} ₽</span>
+        </div>
+        <div class="flex gap-2">
+          <span>Налог 5%:</span>
+          <span class="flex-1 border-b border-dashed border-[#dfdfdf]"></span>
+          <span class="font-semibold">{{ vatPrice }} ₽</span>
+        </div>
+        <button
+          @click="emit('createOrder')"
+          :disabled="isButtonDisabled"
+          class="w-full p-4 mt-4 bg-lime-500 text-white rounded-xl hover:bg-lime-600 active:bg-lime-700 transition disabled:bg-slate-400 cursor-pointer disabled:cursor-not-allowed"
+        >
+          {{ isCreatingOrder ? 'Оформляем...' : 'Оформить заказ' }}
+        </button>
       </div>
-      <div class="flex gap-2">
-        <span>Налог 5%:</span>
-        <span class="flex-1 border-b border-dashed border-[#dfdfdf]"></span>
-        <span class="font-semibold">{{ vatPrice }} ₽</span>
-      </div>
-      <button
-        @click="emit('createOrder')"
-        :disabled="isButtonDisabled"
-        class="w-full p-4 mt-4 bg-lime-500 text-white rounded-xl hover:bg-lime-600 active:bg-lime-700 transition disabled:bg-slate-400 cursor-pointer disabled:cursor-not-allowed"
-      >
-        {{ isCreatingOrder ? 'Оформляем...' : 'Оформить заказ' }}
-      </button>
     </div>
   </div>
 </template>
